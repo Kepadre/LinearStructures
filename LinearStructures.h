e6a51a015533993c39bb0bf003dae4995749cf48
Danil Gaponenko <aloepand@gmail.com>
2023-11-02 16:34:55 +0500

    Added new Queue class

--- LinearStructures.h
+++ LinearStructures.h
@@ -19,3 +19,23 @@ public:
 	bool IsFull(void);
 	void Print(void);
 };
+
+class Queue
+{
+private:
+	int* bottom;
+	int* top;
+	int size;
+
+public:
+	Queue(int);
+	~Queue(void);
+
+	void Enqueue(int);
+	int Dequeue(void);
+	int Peek(void);
+	int Count(void);
+	bool IsEmpty(void);
+	bool IsFull(void);
+	void Print(void);
+};
